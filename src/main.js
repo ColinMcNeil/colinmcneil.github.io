65b5582dff9c2ccf619e2f8e52be2fa4ce8f8154
Colin <colinmcneil@gmail.com>
2018-04-21 20:20:36 -0400

    Create menu page

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
+var menuComponent = new Vue({
+    el: "#menu",
+    data() {
+        return { loaded: false }
+    },
+    methods: {
+        load() {
+            this.loaded = true;
+            titleComponent.loaded = true;
+            titleComponent.loading = false;
+        }
+    }
+})
 var titleComponent = new Vue({
     el: "#title",
     data() {   
-        return {ready:false,loading:false}
+        return { ready: false, loading: false, loaded:false}
         
     },
     methods: {
@@ -16,9 +29,12 @@ var titleComponent = new Vue({
         clicked() {
             if (!this.ready) return;
             this.loading = true;
+            setTimeout(menuComponent.load,1000)
         }
     }
 })
+
+
 var svg = document.getElementById("owlSVG");
 var s = Snap(svg);
 var myname = Snap.select('#myname');
@@ -30,10 +46,10 @@ var toOwl = function () {
     titleComponent.ready = false
     if(titleComponent.animating)owl.stop()
     console.log('derp')
-    owl.animate({ d: owlPoints }, 900, );  
+    owl.animate({ d: owlPoints }, 1200, mina.linear() );  
 }
 var toName = function () {
     if (titleComponent.animating)owl.stop()
     console.log('herp')
-    owl.animate({ d: namePoints }, 900,()=>{titleComponent.ready=true}); 
+    owl.animate({ d: namePoints }, 1200, mina.linear(),()=>{titleComponent.ready=true}); 
 }
