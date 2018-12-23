3057cd7c951ebb7a7509126cf2c13bd344864582
Colin McNeil <colinmcneil@gmail.com>
2018-12-23 13:46:50 -0500

    Remove router base

--- src/nuxt.config.js
+++ src/nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   plugins: [
     { src: '~plugins/mina-vue.js', ssr: false }
   ],
-  router: {
-    base: process.env.NODE_ENV=='production' ? '/colinmcneil.me/' : '/'
-  }
+  // router: {
+  //   base: process.env.NODE_ENV=='production' ? '/colinmcneil.me/' : '/'
+  // }
 }
