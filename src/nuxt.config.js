d8e0bfd71c162d377d9206076a2178b2f0c41e29
Colin McNeil <colinmcneil@gmail.com>
2018-12-23 13:42:16 -0500

    Fix router base

--- src/nuxt.config.js
+++ src/nuxt.config.js
@@ -1,5 +1,8 @@
 export default {
   plugins: [
     { src: '~plugins/mina-vue.js', ssr: false }
-  ]
+  ],
+  router: {
+    base: process.env.NODE_ENV=='production' ? '/colinmcneil.me/' : '/'
+  }
 }
