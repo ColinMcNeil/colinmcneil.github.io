6efe028d8a9c0c873ee21d1c71f58a655b9dbbd6
Colin McNeil <colinmcneil@gmail.com>
2018-12-24 20:05:43 -0500

    Add head tags

--- src/nuxt.config.js
+++ src/nuxt.config.js
@@ -5,6 +5,13 @@ export default {
   modules: [
     'nuxt-webfontloader',
   ],
+  head: {
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {title: 'Colin McNeil'}
+    ]
+  },
   webfontloader: {
     google: {
       families: ['Poiret+One', 'Inconsolata'] //Loads Lato font with weights 400 and 700
